dcad9ffe8b3e04ea212c6fff2c05123e7b7ef8f6
dev_kkshin044 <044apde@gmail.com>
2023-01-18 17:03:12 +0900

    add audio player

--- js/auotoMove.js
+++ js/auotoMove.js
@@ -1,11 +1,23 @@
-const downArrow = document.querySelector("#downArrow");
-downArrow.addEventListener("click", function () {
+const con0downArrow = document.querySelector(".container0 #downArrow");
+con0downArrow.addEventListener("click", function () {
+  document.querySelector("body").style.overflow = "visible";
+  document.querySelector(".container1").scrollIntoView(true);
+});
+
+const con1ownArrow = document.querySelector(".container1 .footer");
+con1ownArrow.addEventListener("click", function () {
   document.querySelector("body").style.overflow = "visible";
   document.querySelector(".container2").scrollIntoView(true);
 });
 
-const upArrow = document.querySelector("#upArrow");
-upArrow.addEventListener("click", function () {
+const con1upArrow = document.querySelector(".container1 .header");
+con1upArrow.addEventListener("click", function () {
+  document.querySelector("body").style.overflow = "visible";
+  document.querySelector(".container0").scrollIntoView(true);
+});
+
+const con2upArrow = document.querySelector(".container2 #upArrow");
+con2upArrow.addEventListener("click", function () {
   document.querySelector("body").style.overflow = "visible";
   document.querySelector(".container1").scrollIntoView(true);
 });
